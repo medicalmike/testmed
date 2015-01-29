419bf576d86ec0b6e1056c76d2171878aba75381
michael.malloy <michael.malloy@medmont.com>
2015-01-29 14:42:32 +1100

    added so many for loops [IS-43]

--- test.c
+++ test.c
@@ -1,18 +1,18 @@
 int main(void){
  int i;
-
+ // comments are good here
  	for(i=0;i<33;i++) {
  		printf("This prints 33 times");
  	}
-
+ 	// commenting helps a lot
  	for(i=0;i<12;i++) {
  		printf("this prints 12 times");
  	}
-
+ 	// is this a form of documentation 
  	for(i=0;i<6;i++) {
  		printf("this prints 6 times");
  	}
-
+ 	// no
  	for(i=0;i<3;i++) {
  		printf("this prints 6 times");
  	}
